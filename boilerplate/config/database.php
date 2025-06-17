2401d4233112db3d92bc1ccda12b09ae8af75d02
mirzaais <mirzamark177@gmail.com>
2025-06-17 19:13:11 +0700

    set up config

--- boilerplate/config/database.php
+++ boilerplate/config/database.php
@@ -1,25 +1,37 @@
 <?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
 class Database {
-    private $host = "localhost";
-    private $database = "dbgame";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $database;
+    private $username;
+    private $password;
+    private $port;
     public $conn;
+
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->port = $_ENV['DB_PORT'];
+        $this->database = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+    }
 
     public function dbConnection(){
         $this->conn = null;
-        try{
-            $this->conn = new PDO(
-                "mysql:host=$this->host;
-                dbname=$this->database",
-                $this->username,
-                $this->password
-            );
+        try {
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->database}";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e){
+            die("Connection failed: " . $e->getMessage());
         }
-        catch(PDOException $exception){
-            die("Connection error: " . $exception->getMessage());
-        }
+
         return $this->conn;
     }
 }
