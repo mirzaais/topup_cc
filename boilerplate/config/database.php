320f54d7ec0615d60144bcfb03e1ad17b5f3c15d
mirzaais <mirzamark177@gmail.com>
2025-06-17 19:26:16 +0700

    set up config

--- boilerplate/config/database.php
+++ boilerplate/config/database.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 class Database {
@@ -35,4 +34,3 @@ class Database {
         return $this->conn;
     }
 }
-?>
